4dff242cc66f7ac21c23348c9aee64c2e52d3077
Scooletz <scooletz@gmail.com>
2016-02-11 11:48:39 +0100

    TestIndependenceBehavior used instead of ClearAsync all the queues for clean up purposes

--- src/AcceptanceTests/ConfigureAzureStorageQueueTransport.cs
+++ src/AcceptanceTests/ConfigureAzureStorageQueueTransport.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Queue;
 using NServiceBus;
 using NServiceBus.AcceptanceTesting.Support;
 using NServiceBus.Azure.Transports.WindowsAzureStorageQueues;
+using NServiceBus.AzureStorageQueues.AcceptanceTests;
 
 class ConfigureAzureStorageQueueTransport : IConfigureTestExecution
 {
-    string connectionString;
-
     public Task Configure(BusConfiguration configuration, IDictionary<string, string> settings)
     {
-        connectionString = settings["Transport.ConnectionString"];
+        var connectionString = settings["Transport.ConnectionString"];
         configuration.UseSerialization<JsonSerializer>();
         configuration.UseTransport<AzureStorageQueueTransport>()
             .ConnectionString(connectionString)
@@ -21,29 +18,16 @@ class ConfigureAzureStorageQueueTransport : IConfigureTestExecution
             .SerializeMessageWrapperWith(defintion => MessageWrapperSerializer.Json.Value)
             .CreateSendingQueues();
 
+        configuration.RegisterComponents(c => { c.ConfigureComponent<TestIndependenceMutator>(DependencyLifecycle.SingleInstance); });
+        configuration.Pipeline.Register("TestIndependenceBehavior", typeof(TestIndependenceSkipBehavior),
+            "Skips messages not created during the current test.");
+
         return Task.FromResult(0);
     }
 
-    public async Task Cleanup()
+    public Task Cleanup()
     {
-        var storage = CloudStorageAccount.Parse(connectionString);
-        var queues = storage.CreateCloudQueueClient();
-        var cloudQueues = new Queue<CloudQueue>(queues.ListQueues());
-
-        while (cloudQueues.Count > 0)
-        {
-            const int batchSize = 32;
-            var tasks = new List<Task>();
-            for (var i = 0; i < batchSize; i++)
-            {
-                if (cloudQueues.Count > 0)
-                {
-                    var queue = cloudQueues.Dequeue();
-                    tasks.Add(queue.ClearAsync());
-                }
-            }
-
-            await Task.WhenAll(tasks).ConfigureAwait(false);
-        }
+        // nothing to clean as TestIndependenceSkipBehavior is registered
+        return Task.FromResult(0);
     }
 }
