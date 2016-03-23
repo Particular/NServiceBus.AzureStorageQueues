3135e2216efec9a6ae32f0c4440fecdcc657cf6b
Sean Feldman <feldman.sean@gmail.com>
2016-03-23 13:02:02 -0600

    added API approvals to control public API

---
+++ src/Tests/API/APIApprovals.cs
@@ -0,0 +1,25 @@
+﻿namespace NServiceBus.Azure.WindowsAzureServiceBus.Tests.API
+{
+    using System.IO;
+    using System.Runtime.CompilerServices;
+    using ApiApprover;
+    using ApprovalTests;
+    using ApprovalTests.Reporters;
+    using Mono.Cecil;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class APIApprovals
+    {
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        [UseReporter(typeof(DiffReporter))]
+        public void ApproveAzureStorageQueueTransport()
+        {
+            var assemblyPath = Path.GetFullPath(typeof(AzureStorageQueueTransport).Assembly.Location);
+            var asm = AssemblyDefinition.ReadAssembly(assemblyPath);
+            var publicApi = PublicApiGenerator.CreatePublicApiForAssembly(asm, definition => true, false);
+            Approvals.Verify(publicApi);
+        }
+    }
+}
