ae68c895808134d8d1b2a87bc1c6803f02973445
Michał Wójcik <wojcik.mike@gmail.com>
2016-04-20 11:31:17 +0200

    Making ConnectionString internal

--- src/Transport/ConnectionString.cs
+++ src/Transport/ConnectionString.cs
@@ -2,7 +2,7 @@
 {
     using System;
 
-    public sealed class ConnectionString : IEquatable<ConnectionString>
+    sealed class ConnectionString : IEquatable<ConnectionString>
     {
         public ConnectionString(string value)
         {
