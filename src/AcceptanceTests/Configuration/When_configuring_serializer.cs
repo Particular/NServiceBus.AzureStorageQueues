a784031b007cf59c8f7adfe8793a58d5e9f41c61
Tim Bussmann <tim.bussmann@particular.net>
2016-04-25 15:45:48 +0200

    add acceptance test testing json default

---
+++ src/AcceptanceTests/Configuration/When_configuring_serializer.cs
@@ -0,0 +1,49 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Features;
+    using NUnit.Framework;
+    using Serialization;
+
+    public class When_configuring_serializer : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_use_configured_serializer()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<CustomConfigurationEndpoint>()
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.IsInstanceOf<XmlSerializer>(context.Serializer);
+        }
+
+        class Context : ScenarioContext
+        {
+            public SerializationDefinition Serializer { get; set; }
+        }
+
+        class CustomConfigurationEndpoint : EndpointConfigurationBuilder
+        {
+            public CustomConfigurationEndpoint()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.EnableFeature<SerializationConfigFeature>();
+                    c.UseSerialization<XmlSerializer>();
+                });
+            }
+
+            class SerializationConfigFeature : Feature
+            {
+                protected override void Setup(FeatureConfigurationContext context)
+                {
+                    var testContext = context.Settings.Get<ScenarioContext>() as Context;
+                    testContext.Serializer = context.Settings.Get<SerializationDefinition>();
+                }
+            }
+        }
+    }
+}
---
+++ src/AcceptanceTests/Configuration/When_not_configuring_serializer.cs
@@ -0,0 +1,45 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Features;
+    using NUnit.Framework;
+    using Serialization;
+
+    public class When_not_configuring_serializer : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_use_JSON_serializer()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<DefaultConfigurationEndpoint>()
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            Assert.IsInstanceOf<JsonSerializer>(context.Serializer);
+        }
+
+        class Context : ScenarioContext
+        {
+            public SerializationDefinition Serializer { get; set; }
+        }
+
+        class DefaultConfigurationEndpoint : EndpointConfigurationBuilder
+        {
+            public DefaultConfigurationEndpoint()
+            {
+                EndpointSetup<DefaultServer>(c => c.EnableFeature<SerializationConfigFeature>());
+            }
+
+            class SerializationConfigFeature : Feature
+            {
+                protected override void Setup(FeatureConfigurationContext context)
+                {
+                    var testContext = context.Settings.Get<ScenarioContext>() as Context;
+                    testContext.Serializer = context.Settings.Get<SerializationDefinition>();
+                }
+            }
+        }
+    }
+}
