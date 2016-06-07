d3f90fafc27a1fb463c8273b7c9e6f3a2bddc697
Tim Bussmann <tim.bussmann@particular.net>
2016-06-07 13:28:42 +0200

    register message wrapper as system message

--- src/Transport/AzureStorageQueueTransport.cs
+++ src/Transport/AzureStorageQueueTransport.cs
@@ -1,5 +1,6 @@
 namespace NServiceBus
 {
+    using Azure.Transports.WindowsAzureStorageQueues;
     using AzureStorageQueues;
     using Serialization;
     using Settings;
@@ -19,6 +20,9 @@ namespace NServiceBus
         {
             // configure JSON instead of XML as the default serializer:
             settings.SetDefault<SerializationDefinition>(new JsonSerializer());
+
+            // register the MessageWrapper as a system message to have it registered in mappings and serializers
+            settings.Get<Conventions>().AddSystemMessagesConventions(t => t == typeof(MessageWrapper));
 
             new DefaultConfigurationValues().Apply(settings);
 
