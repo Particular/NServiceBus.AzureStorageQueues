9c09de10a775d8daff1f5737c896506e6232e632
Daniel Marbach <daniel.marbach@openplace.net>
2016-06-30 13:50:25 +0000

    Queue address generator (#133)
    * Using compiled and cached regex
    * Caching of sanitized queue names

--- src/Transport/QueueAddressGenerator.cs
+++ src/Transport/QueueAddressGenerator.cs
@@ -1,6 +1,7 @@
 ﻿namespace NServiceBus.AzureStorageQueues
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Text.RegularExpressions;
     using Config;
     using Settings;
@@ -21,28 +22,36 @@
 
         public string GetQueueName(string address)
         {
-            var name = SanitizeQueueName(address.ToLowerInvariant());
-            var input = address.Replace('.', '-').ToLowerInvariant(); // this string was used in the past to calculate guid, should stay backward compat
+            var queueName = address.ToLowerInvariant();
 
-            if (name.Length > 63)
+            return sanitizedQueueNames.GetOrAdd(queueName, name => ShortenQueueNameIfNecessary(name, SanitizeQueueName(name)));
+        }
+
+        string ShortenQueueNameIfNecessary(string address, string queueName)
+        {
+            if (queueName.Length <= 63)
             {
-                var shortenedName = shortener(input);
-                name = name.Substring(0, 63 - shortenedName.Length - 1).Trim('-') + "-" + shortenedName;
+                return queueName;
             }
 
-            return name;
+            var input = address.Replace('.', '-').ToLowerInvariant(); // this string was used in the past to calculate guid, should stay backward compat
+            var shortenedName = shortener(input);
+            queueName = $"{queueName.Substring(0, 63 - shortenedName.Length - 1).Trim('-')}-{shortenedName}";
+            return queueName;
         }
 
         static string SanitizeQueueName(string queueName)
         {
             //rules for naming queues can be found at http://msdn.microsoft.com/en-us/library/windowsazure/dd179349.aspx"
-            var invalidCharacters = new Regex(@"[^a-zA-Z0-9\-]");
             var sanitized = invalidCharacters.Replace(queueName, "-"); // this can lead to multiple - occurrences in a row
-            var multipleDashes = new Regex(@"\-+");
             sanitized = multipleDashes.Replace(sanitized, "-");
             return sanitized;
         }
 
         Func<string, string> shortener;
+        ConcurrentDictionary<string, string> sanitizedQueueNames = new ConcurrentDictionary<string, string>();
+
+        static Regex invalidCharacters = new Regex(@"[^a-zA-Z0-9\-]", RegexOptions.Compiled);
+        static Regex multipleDashes = new Regex(@"\-+", RegexOptions.Compiled);
     }
 }
