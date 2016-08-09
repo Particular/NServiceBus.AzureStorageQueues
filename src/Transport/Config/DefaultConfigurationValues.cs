9c86cf5003c8b0d9c1994d48d5bac0e51c88fdb5
Szymon Kulec <scooletz@gmail.com>
2016-08-09 10:12:15 +0200

    Increase default DefaultPeekInterval

--- src/Transport/Config/DefaultConfigurationValues.cs
+++ src/Transport/Config/DefaultConfigurationValues.cs
@@ -21,7 +21,7 @@
         }
 
         TimeSpan DefaultMessageInvisibleTime = TimeSpan.FromSeconds(30);
-        TimeSpan DefaultPeekInterval = TimeSpan.FromMilliseconds(50);
+        TimeSpan DefaultPeekInterval = TimeSpan.FromMilliseconds(125);
         TimeSpan DefaultMaximumWaitTimeWhenIdle = TimeSpan.FromSeconds(1);
         const int DefaultBatchSize = 32;
         const string DefaultConnectionString = "";
