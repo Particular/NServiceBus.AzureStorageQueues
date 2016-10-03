b96ef72240c9419eeb625230a249fab687c62326
Szymon Kulec <scooletz@gmail.com>
2016-10-03 15:43:53 +0200

    Fix for Transport Seam Tests.

--- src/NServiceBus.AzureStorageQueues.TransportTests/ConfigureAzureStorageQueueTransportInfrastructure.cs
+++ src/NServiceBus.AzureStorageQueues.TransportTests/ConfigureAzureStorageQueueTransportInfrastructure.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+using System.Globalization;
+using System.Reflection;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Azure.Transports.WindowsAzureStorageQueues.TransportTests;
 using NServiceBus.Logging;
 using NServiceBus.Settings;
 using NServiceBus.TransportTests;
+using NServiceBus.Unicast.Messages;
 
 public class ConfigureAzureStorageQueueTransportInfrastructure : IConfigureTransportInfrastructure
 {
     public TransportConfigurationResult Configure(SettingsHolder settings, TransportTransactionMode transactionMode)
     {
         LogManager.UseFactory(new ConsoleLoggerFactory());
+
+        MessageMetadataRegistry registry;
+        if (settings.TryGet(out registry) == false)
+        {
+            const BindingFlags flags = BindingFlags.Instance | BindingFlags.Public |BindingFlags.NonPublic | BindingFlags.CreateInstance;
+
+            registry = (MessageMetadataRegistry) Activator.CreateInstance(typeof(MessageMetadataRegistry), flags, null, new object[] {settings.GetOrCreate<Conventions>()}, CultureInfo.InvariantCulture);
+            settings.Set<MessageMetadataRegistry>(registry);
+        }
 
         return new TransportConfigurationResult
         {
