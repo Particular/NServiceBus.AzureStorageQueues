220676c3ed6de149fc94430c8c6aca622815375e
andreasohlund <andreas.ohlund@particular.net>
2016-12-01 12:01:05 +0100

    Make the unwrapper interface internal

--- src/Transport/IMessageEnvelopeUnwrapper.cs
+++ src/Transport/IMessageEnvelopeUnwrapper.cs
@@ -3,16 +3,8 @@ namespace NServiceBus.AzureStorageQueues
     using Azure.Transports.WindowsAzureStorageQueues;
     using Microsoft.WindowsAzure.Storage.Queue;
 
-    /// <summary>
-    /// Extracts a native cloud queue message to the internal message wrapper format.
-    /// </summary>
-    public interface IMessageEnvelopeUnwrapper
+    interface IMessageEnvelopeUnwrapper
     {
-        /// <summary>
-        /// Unwraps the given message.
-        /// </summary>
-        /// <param name="rawMessage">The raw cloud queue message received from the Azure storage queue.</param>
-        /// <returns></returns>
         MessageWrapper Unwrap(CloudQueueMessage rawMessage);
     }
 }
