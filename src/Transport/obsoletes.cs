d983cef3811fb68f9473010d5c0a686abf360348
Sean Feldman <feldman.sean@gmail.com>
2017-09-06 14:44:47 -0600

    approving modified API

--- src/Transport/obsoletes.cs
+++ src/Transport/obsoletes.cs
@@ -7,8 +7,8 @@
         /// <summary>
         /// Overrides default Md5 shortener for creating queue names with Sha1 shortener.
         /// </summary>
-        [ObsoleteEx(Message = "Azure Storage Queues transport is no longer shortening queues and requires shortening algorithm to be provided using configuration API.",
-            ReplacementTypeOrMember = "transport.UseNameShortener(Func<string, string>)",
+        [ObsoleteEx(Message = "Azure Storage Queues transport is no longer shortening queue names and requires sanitization algorithm to be provided using configuration API.",
+            ReplacementTypeOrMember = "transport.SanitizeQueueNamesWith(Func<string, string>)",
             TreatAsErrorFromVersion = "8.0", RemoveInVersion = "9.0")]
         public static TransportExtensions<AzureStorageQueueTransport> UseSha1ForShortening(this TransportExtensions<AzureStorageQueueTransport> config)
         {
