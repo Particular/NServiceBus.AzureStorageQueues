0b180b65cc742db94d0200e758d7a89529c85e2d
Simon Cropp <simon.cropp@gmail.com>
2017-09-07 13:18:50 +1000

    remove redundant Shortener methods

--- src/AcceptanceTests/BackwardsCompatibleQueueNameSanitizerForTests.cs
+++ src/AcceptanceTests/BackwardsCompatibleQueueNameSanitizerForTests.cs
@@ -18,7 +18,7 @@ static class BackwardsCompatibleQueueNameSanitizerForTests
             return queueName;
         }
 
-        var shortenedName = Shortener.Md5(address);
+        var shortenedName = ShortenWithMd5(address);
         queueName = $"{queueName.Substring(0, 63 - shortenedName.Length - 1).Trim('-')}-{shortenedName}";
         return queueName;
     }
@@ -34,37 +34,15 @@ static class BackwardsCompatibleQueueNameSanitizerForTests
     static Regex invalidCharacters = new Regex(@"[^a-zA-Z0-9\-]", RegexOptions.Compiled);
     static Regex multipleDashes = new Regex(@"\-+", RegexOptions.Compiled);
 
-    class Shortener
+    static string ShortenWithMd5(string test)
     {
-        public static string Md5(string test)
+        //use MD5 hash to get a 16-byte hash of the string
+        using (var provider = MD5.Create())
         {
-            //use MD5 hash to get a 16-byte hash of the string
-            using (var provider = MD5.Create())
-            {
-                var inputBytes = Encoding.Default.GetBytes(test);
-                var hashBytes = provider.ComputeHash(inputBytes);
-                //generate a guid from the hash:
-                return new Guid(hashBytes).ToString();
-            }
-        }
-
-        static string ToChars(byte[] hashBytes)
-        {
-            var chars = new char[hashBytes.Length * 2];
-            for (var i = 0; i < chars.Length; i += 2)
-            {
-                var byteIndex = i / 2;
-                chars[i] = HexToChar((byte)(hashBytes[byteIndex] >> 4));
-                chars[i + 1] = HexToChar(hashBytes[byteIndex]);
-            }
-
-            return new string(chars);
-        }
-
-        static char HexToChar(byte a)
-        {
-            a &= 15;
-            return a > 9 ? (char)(a - 10 + 97) : (char)(a + 48);
+            var inputBytes = Encoding.Default.GetBytes(test);
+            var hashBytes = provider.ComputeHash(inputBytes);
+            //generate a guid from the hash:
+            return new Guid(hashBytes).ToString();
         }
     }
 }
