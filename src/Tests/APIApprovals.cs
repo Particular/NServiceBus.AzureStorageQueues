f2bd395c6271d601154c1150bc045c95b9d1bb81
Sean Feldman <feldman.sean@gmail.com>
2017-09-06 20:50:16 -0600

    removing namespace

--- src/Tests/APIApprovals.cs
+++ src/Tests/APIApprovals.cs
@@ -1,28 +1,25 @@
 ﻿#if NET452
 
-namespace NServiceBus.Azure.AzureStorageQueues.Tests.API
-{
-    using System.IO;
-    using System.Reflection;
-    using System.Runtime.CompilerServices;
-    using ApprovalTests;
-    using ApprovalTests.Reporters;
-    using NUnit.Framework;
-    using PublicApiGenerator;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using PublicApiGenerator;
 
-    [TestFixture]
-    public class APIApprovals
+[TestFixture]
+public class APIApprovals
+{
+    [Test]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    [UseReporter(typeof(DiffReporter), typeof(AllFailingTestsClipboardReporter))]
+    public void ApproveAzureStorageQueueTransport()
     {
-        [Test]
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        [UseReporter(typeof(DiffReporter), typeof(AllFailingTestsClipboardReporter))]
-        public void ApproveAzureStorageQueueTransport()
-        {
-            var combine = Path.Combine(TestContext.CurrentContext.TestDirectory, "NServiceBus.Azure.Transports.WindowsAzureStorageQueues.dll");
-            var assembly = Assembly.LoadFile(combine);
-            var publicApi = ApiGenerator.GeneratePublicApi(assembly);
-            Approvals.Verify(publicApi);
-        }
+        var combine = Path.Combine(TestContext.CurrentContext.TestDirectory, "NServiceBus.Azure.Transports.WindowsAzureStorageQueues.dll");
+        var assembly = Assembly.LoadFile(combine);
+        var publicApi = ApiGenerator.GeneratePublicApi(assembly);
+        Approvals.Verify(publicApi);
     }
 }
 
