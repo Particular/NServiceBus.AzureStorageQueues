5eecf9acfd242b948d4fdc14292bbeab672e2d32
Sean Feldman <feldman.sean@gmail.com>
2017-09-14 22:38:32 -0600

    setting keys for delayed delivery

--- src/Transport/Config/WellKnownConfigurationKeys.cs
+++ src/Transport/Config/WellKnownConfigurationKeys.cs
@@ -11,5 +11,12 @@
         public const string DegreeOfReceiveParallelism = "Transport.AzureStorageQueue.DegreeOfReceiveParallelism";
         public const string UseAccountNamesInsteadOfConnectionStrings = "Transport.AzureStorageQueue.UseAccountAliasesInsteadOfConnectionStrings";
         public const string QueueSanitizer = "Transport.AzureStorageQueue.QueueSanitizer";
+
+        public static class DelayedDelivery
+        {
+            public const string TableName = "Transport.AzureStorageQueue.TableName";
+            public const string DisableTimeoutManager = "Transport.AzureStorageQueue.DisableTimeoutManager";
+            public const string DisableDelayedDelivery = "Transport.AzureStorageQueue.DisableDelayedDelivery";
+        }
     }
 }
