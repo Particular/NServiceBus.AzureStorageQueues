0034f15ead6b330d819095d5cdfad2a275c668ac
Kyle Baley <kyle@baley.org>
2017-09-21 17:08:04 -0500

    Output connection string

--- src/LinuxTest/Connect_to_azure.cs
+++ src/LinuxTest/Connect_to_azure.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 using NUnit.Framework;
+using System;
 
 [TestFixture]
 [Category("Azure")]
@@ -11,6 +12,7 @@ public class Connect_to_Azure
     [Test]
     public async Task Should_parse_namespace_from_azure_storage_connectionstring()
     {
+        Console.WriteLine("    Connection String :: " + Utils.GetEnvConfiguredConnectionString());
         var cloudStorageAccount = CloudStorageAccount.Parse(Utils.GetEnvConfiguredConnectionString());
         var queueClient = cloudStorageAccount.CreateCloudQueueClient();
         var result = await queueClient.ListQueuesSegmentedAsync(new QueueContinuationToken());
