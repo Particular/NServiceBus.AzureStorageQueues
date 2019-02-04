ee0b1f3865c1c173555fd8ab8fce479d2f0715a6
danielmarbach <daniel.marbach@openplace.net>
2019-02-04 14:10:23 +0100

    Allow to control batch size and move received message list allocation one level up

--- src/Transport/AzureStorageQueueInfrastructure.cs
+++ src/Transport/AzureStorageQueueInfrastructure.cs
@@ -104,8 +104,6 @@
                     var receiver = new AzureMessageQueueReceiver(unwrapper, client, addressGenerator)
                     {
                         MessageInvisibleTime = settings.Get<TimeSpan>(WellKnownConfigurationKeys.ReceiverMessageInvisibleTime),
-
-                        BatchSize = settings.Get<int>(WellKnownConfigurationKeys.ReceiverBatchSize)
                     };
 
                     int? degreeOfReceiveParallelism = null;
@@ -114,7 +112,13 @@
                         degreeOfReceiveParallelism = parallelism;
                     }
 
-                    return new MessagePump(receiver, addressing, degreeOfReceiveParallelism, maximumWaitTime, peekInterval);
+                    int? batchSize = null;
+                    if (settings.TryGet<int>(WellKnownConfigurationKeys.ReceiverBatchSize, out var size))
+                    {
+                        batchSize = size;
+                    }
+
+                    return new MessagePump(receiver, addressing, degreeOfReceiveParallelism, batchSize, maximumWaitTime, peekInterval);
                 },
                 () => new AzureMessageQueueCreator(client, addressGenerator),
                 () => Task.FromResult(StartupCheckResult.Success)
