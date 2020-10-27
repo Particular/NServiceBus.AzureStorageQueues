24d0d50c5d3e5af6271d6f60db09db4bb45d78df
Sean Feldman <feldman.sean@gmail.com>
2020-10-27 00:08:10 -0600

    Update error message in the test to reflect exception message thrown

--- src/AcceptanceTests/Configuration/When_configuring_account_names.cs
+++ src/AcceptanceTests/Configuration/When_configuring_account_names.cs
@@ -24,7 +24,7 @@
         public void Should_not_accept_mappings_without_default()
         {
             var exception = Assert.CatchAsync(() => { return Configure(cfg => { cfg.AccountRouting().AddAccount(Another, anotherConnectionString); }); });
-            Assert.IsTrue(exception.Message.Contains("The mapping of account names instead of connection strings is enabled, but the default connection string name isn\'t provided. Provide the default connection string name when adding more accounts"), "Message is missing or incorrect");
+            Assert.IsTrue(exception.Message.Contains("The mapping of account names instead of connection strings is enabled but the default connection string name isn\'t provided. Provide the default connection string name when adding more accounts"), "Message is missing or incorrect");
         }
 
         [Test]
