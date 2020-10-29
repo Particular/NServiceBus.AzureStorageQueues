6d35594cd54187349e49387e2642ffbdf6bcc62a
Sean Feldman <feldman.sean@gmail.com>
2020-10-28 23:20:46 -0600

    Add verification we cannot dispatch using connection string instead of alias

---
+++ src/AcceptanceTests/Sending/When_dispatching_to_other_account_using_connection_string.cs
@@ -0,0 +1,98 @@
+﻿namespace NServiceBus.AcceptanceTests.WindowsAzureStorageQueues.Sending
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using AcceptanceTesting.Customization;
+    using EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_dispatching_to_other_account_using_connection_string : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public Task Should_fail()
+        {
+            var exception = Assert.ThrowsAsync<Exception>(async () =>
+            {
+                await Scenario.Define<Context>()
+                    .WithEndpoint<Sender>(b =>
+                    {
+                        b.When((bus, c) =>
+                        {
+                            var options = new SendOptions();
+                            options.SetDestination(
+                                $"{Conventions.EndpointNamingConvention(typeof(Receiver))}@{ConfigureEndpointAzureStorageQueueTransport.ConnectionString}");
+                            return bus.Send(new MyMessage(), options);
+                        });
+                    })
+                    .WithEndpoint<Receiver>()
+                    .Done(c => c.WasCalled)
+                    .Run().ConfigureAwait(false);
+            });
+
+            Assert.IsTrue(exception.Message.Contains("An attempt to use an address with a connection string using the 'destination@connecitonstring' format was detected. Only aliases are allowed. Provide an alias for the storage account."));
+
+            return Task.CompletedTask;
+        }
+
+        const string Alias = "another";
+        const string DefaultAccountName = "default";
+
+        public class Context : ScenarioContext
+        {
+            public string SendTo { get; set; }
+            public bool WasCalled { get; set; }
+        }
+
+        class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(configuration =>
+                {
+                    configuration.UseTransport<AzureStorageQueueTransport>()
+                        .DefaultAccountAlias(DefaultAccountName)
+                        .ConnectionString(ConfigureEndpointAzureStorageQueueTransport.ConnectionString)
+                        .AccountRouting()
+                        .AddAccount(Alias, ConfigureEndpointAzureStorageQueueTransport.AnotherConnectionString);
+
+                    configuration.ConfigureTransport().Routing().RouteToEndpoint(typeof(MyMessage), typeof(Receiver));
+                });
+            }
+        }
+
+        public class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                EndpointSetup<DefaultServer>(configuration =>
+                {
+                    configuration.UseTransport<AzureStorageQueueTransport>()
+                        .DefaultAccountAlias(Alias)
+                        .ConnectionString(ConfigureEndpointAzureStorageQueueTransport.AnotherConnectionString);
+                });
+            }
+
+            public class MyMessageHandler : IHandleMessages<MyMessage>
+            {
+                Context testContext;
+
+                public MyMessageHandler(Context testContext)
+                {
+                    this.testContext = testContext;
+                }
+
+                public Task Handle(MyMessage message, IMessageHandlerContext context)
+                {
+                    testContext.WasCalled = true;
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        [Serializable]
+        public class MyMessage : ICommand
+        {
+        }
+    }
+}
