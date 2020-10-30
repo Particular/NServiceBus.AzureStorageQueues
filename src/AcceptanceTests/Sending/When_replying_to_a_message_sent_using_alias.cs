67650e3f18ba33a3a8674df7e401b96fc627d654
danielmarbach <daniel.marbach@openplace.net>
2020-10-30 16:57:15 +0100

    Comment out routing entry with TODO

--- src/AcceptanceTests/Sending/When_replying_to_a_message_sent_using_alias.cs
+++ src/AcceptanceTests/Sending/When_replying_to_a_message_sent_using_alias.cs
@@ -87,7 +87,8 @@
                     // Route MyMessage messages to the receiver endpoint configured to use sender alias (on a different storage account)
                     var senderEndpointName = Conventions.EndpointNamingConvention(typeof(Sender));
                     senderEndpointAccountInfo.RegisteredEndpoints.Add(senderEndpointName);
-                    configuration.ConfigureTransport().Routing().RouteToEndpoint(typeof(MyReplyMessage), senderEndpointName);
+                    // TODO: remove if not necessary
+                    //configuration.ConfigureTransport().Routing().RouteToEndpoint(typeof(MyReplyMessage), senderEndpointName);
                 });
             }
 
