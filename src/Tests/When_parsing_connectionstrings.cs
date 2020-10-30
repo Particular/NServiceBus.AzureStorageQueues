a25aeeb9df664f80253b9666b22a4d194624c0f2
Sean Feldman <feldman.sean@gmail.com>
2020-10-30 00:38:47 -0600

    Add verification for parsing a queue address with connection string

--- src/Tests/When_parsing_connectionstrings.cs
+++ src/Tests/When_parsing_connectionstrings.cs
@@ -24,6 +24,19 @@ public class When_parsing_queueaddress
         Assert.Throws<Exception>(() => QueueAddress.Parse(queue));
     }
 
+    [Test]
+    public void Should_parse_a_connectionstring_when_instructed_to_support_backward_compatibility()
+    {
+        const string queueName = "myqueue";
+        const string connectionString = "DefaultEndpointsProtocol=https;AccountName=nservicebus;AccountKey=4CBm0byd405DrwMlNGQcHntKDgAQCjaxHNX4mmjMx0p3mNaxrg4Y9zdTVVy0MBzKjQtRKd1M6DF5CwQseBTw/g==";
+        var queue = $"{queueName}@{connectionString}";
+
+        var address = QueueAddress.Parse(queue, true);
+
+        Assert.AreEqual(queueName, address.QueueName);
+        Assert.AreEqual(connectionString, address.Alias);
+    }
+
     [TestCase("@accountName")]
     [TestCase("  @accountName")]
     [TestCase(default(string))]
