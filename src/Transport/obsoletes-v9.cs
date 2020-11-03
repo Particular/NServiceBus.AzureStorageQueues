c385ad9943d42ec6600a8c46a59a9a8f705e554b
Sean Feldman <feldman.sean@gmail.com>
2020-11-03 16:11:09 -0700

    Consolidate obsoletes

--- src/Transport/obsoletes-v9.cs
+++ src/Transport/obsoletes-v9.cs
@@ -10,10 +10,7 @@ namespace NServiceBus
             Message = "Account aliases are used instead of connection strings by default",
             TreatAsErrorFromVersion = "9",
             RemoveInVersion = "10")]
-        public static TransportExtensions<AzureStorageQueueTransport> UseAccountAliasesInsteadOfConnectionStrings(this TransportExtensions<AzureStorageQueueTransport> config)
-        {
-            throw new NotImplementedException();
-        }
+        public static TransportExtensions<AzureStorageQueueTransport> UseAccountAliasesInsteadOfConnectionStrings(this TransportExtensions<AzureStorageQueueTransport> config) => throw new NotImplementedException();
     }
 
     partial class DelayedDeliverySettings
@@ -23,6 +20,19 @@ namespace NServiceBus
             TreatAsErrorFromVersion = "9",
             RemoveInVersion = "10")]
         public void DisableTimeoutManager() => throw new NotImplementedException();
+    }
+
+    public static partial class AzureStorageTransportExtensions
+    {
+        /// <summary>
+        /// Registers a queue name sanitizer to apply to queue names not compliant wth Azure Storage Queue naming rules.
+        /// <remarks>By default no sanitization is performed.</remarks>
+        /// </summary>
+        [ObsoleteEx(
+            Message = "Queue name sanitization should be considered when providing the endpoint logical name using `new EndpointConfiguration(<name>)` API.",
+            RemoveInVersion = "10.0.0",
+            TreatAsErrorFromVersion = "9.0.0")]
+        public static TransportExtensions<AzureStorageQueueTransport> SanitizeQueueNamesWith(this TransportExtensions<AzureStorageQueueTransport> config, Func<string, string> queueNameSanitizer) => throw new NotImplementedException();
     }
 }
 #pragma warning restore 1591
