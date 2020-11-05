73f151caafe8bc55f42855cfbbce347ad27dea96
danielmarbach <daniel.marbach@openplace.net>
2020-11-05 17:40:33 +0100

    Fix after merge

--- src/Transport/obsoletes-v9.cs
+++ src/Transport/obsoletes-v9.cs
@@ -13,15 +13,6 @@ namespace NServiceBus
         public static TransportExtensions<AzureStorageQueueTransport> UseAccountAliasesInsteadOfConnectionStrings(this TransportExtensions<AzureStorageQueueTransport> config) => throw new NotImplementedException();
     }
 
-    partial class DelayedDeliverySettings
-    {
-        [ObsoleteEx(
-            Message = "The timeout manager has been removed in favor of native delayed delivery support provided by transports. See the upgrade guide for more details.",
-            TreatAsErrorFromVersion = "9",
-            RemoveInVersion = "10")]
-        public void DisableTimeoutManager() => throw new NotImplementedException();
-    }
-
     public static partial class AzureStorageTransportExtensions
     {
         [ObsoleteEx(
