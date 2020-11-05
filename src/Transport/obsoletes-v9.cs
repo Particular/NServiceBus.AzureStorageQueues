bdfc71217f1bdce0c35902aef9fdd7e785f56ba1
danielmarbach <daniel.marbach@openplace.net>
2020-11-05 18:09:05 +0100

    Fix obsoletes after version bump

--- src/Transport/obsoletes-v9.cs
+++ src/Transport/obsoletes-v9.cs
@@ -4,31 +4,14 @@ namespace NServiceBus
 {
     using System;
 
-    public static partial class AzureStorageTransportAddressingExtensions
-    {
-        [ObsoleteEx(
-            Message = "Account aliases are used instead of connection strings by default",
-            TreatAsErrorFromVersion = "9",
-            RemoveInVersion = "10")]
-        public static TransportExtensions<AzureStorageQueueTransport> UseAccountAliasesInsteadOfConnectionStrings(this TransportExtensions<AzureStorageQueueTransport> config) => throw new NotImplementedException();
-    }
-
     partial class DelayedDeliverySettings
     {
         [ObsoleteEx(
             Message = "The timeout manager has been removed in favor of native delayed delivery support provided by transports. See the upgrade guide for more details.",
-            TreatAsErrorFromVersion = "9",
-            RemoveInVersion = "10")]
+            TreatAsErrorFromVersion = "10",
+            RemoveInVersion = "11")]
         public void DisableTimeoutManager() => throw new NotImplementedException();
     }
 
-    public static partial class AzureStorageTransportExtensions
-    {
-        [ObsoleteEx(
-            Message = "Queue name sanitization should be considered when providing the endpoint logical name using `new EndpointConfiguration(<name>)` API.",
-            RemoveInVersion = "10.0.0",
-            TreatAsErrorFromVersion = "9.0.0")]
-        public static TransportExtensions<AzureStorageQueueTransport> SanitizeQueueNamesWith(this TransportExtensions<AzureStorageQueueTransport> config, Func<string, string> queueNameSanitizer) => throw new NotImplementedException();
-    }
 }
 #pragma warning restore 1591
