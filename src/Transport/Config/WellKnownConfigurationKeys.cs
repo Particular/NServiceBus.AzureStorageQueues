62df39ff4410050273d0a4141ae4e312204ac4fd
mauroservienti <mauro.servienti@gmail.com>
2021-01-08 11:05:50 +0100

    Remove TODO

--- src/Transport/Config/WellKnownConfigurationKeys.cs
+++ src/Transport/Config/WellKnownConfigurationKeys.cs
@@ -6,8 +6,6 @@
         //public const string ReceiverConnectionString = "Transport.ConnectionString";
         public const string ReceiverMaximumWaitTimeWhenIdle = "Transport.AzureStorageQueue.ReceiverMaximumWaitTimeWhenIdle";
         public const string ReceiverPeekInterval = "Transport.AzureStorageQueue.ReceiverPeekInterval";
-        //TODO: moved to TransportDef.MessageInvisibleTime
-        //public const string ReceiverMessageInvisibleTime = "Transport.AzureStorageQueue.Settings.ReceiverMessageInvisibleTime";
         public const string ReceiverBatchSize = "Transport.AzureStorageQueue.ReceiverBatchSize";
         public const string MessageWrapperSerializationDefinition = "Transport.AzureStorageQueue.MessageWrapperSerializationDefinition";
         public const string DegreeOfReceiveParallelism = "Transport.AzureStorageQueue.DegreeOfReceiveParallelism";
