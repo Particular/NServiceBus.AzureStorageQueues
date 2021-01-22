72b8902a276a6b8a8774d7622028d2f99df6d91d
mauroservienti <mauro.servienti@gmail.com>
2021-01-22 16:59:54 +0100

    Split NativeDelayedDelivery into Persistence and Processor

---
+++ src/Transport/DelayDelivery/NativeDelayDeliveryPersistence.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace NServiceBus.Transport.AzureStorageQueues
+{
+    class NativeDelayDeliveryPersistence
+    {
+        public static NativeDelayDeliveryPersistence Disabled()
+        {
+            return new NativeDelayDeliveryPersistence();
+        }
+
+        private NativeDelayDeliveryPersistence()
+        {
+            enabled = false;
+        }
+
+        public NativeDelayDeliveryPersistence(CloudTable delayedMessageStorageTable)
+        {
+            enabled = true;
+            this.delayedMessageStorageTable = delayedMessageStorageTable;
+        }
+
+        public bool IsDelayedMessage(UnicastTransportOperation operation, out DateTimeOffset dueDate)
+        {
+            var delay = GetDeliveryDelay(operation.Properties);
+            if (delay != null)
+            {
+                if (operation.Properties.DiscardIfNotReceivedBefore != null)
+                {
+                    throw new Exception($"Postponed delivery of messages with TimeToBeReceived set is not supported. Remove the TimeToBeReceived attribute to postpone messages of type '{operation.Message.Headers[Headers.EnclosedMessageTypes]}'.");
+                }
+
+                dueDate = DateTimeOffset.UtcNow + delay.Value;
+
+                return true;
+            }
+
+            dueDate = DateTimeOffset.MinValue;
+            return false;
+        }
+
+        static TimeSpan? GetDeliveryDelay(OperationProperties properties)
+        {
+            var doNotDeliverBefore = properties.DoNotDeliverBefore;
+            if (doNotDeliverBefore != null)
+            {
+                return ToNullIfNegative(doNotDeliverBefore.At - DateTimeOffset.UtcNow);
+            }
+
+            var delay = properties.DelayDeliveryWith;
+            if (delay != null)
+            {
+                return ToNullIfNegative(delay.Delay);
+            }
+
+            return null;
+        }
+
+        static TimeSpan? ToNullIfNegative(TimeSpan value)
+        {
+            return value <= TimeSpan.Zero ? (TimeSpan?)null : value;
+        }
+
+        public Task ScheduleAt(UnicastTransportOperation operation, DateTimeOffset date, CancellationToken cancellationToken)
+        {
+            if (!enabled)
+            {
+                throw new Exception("Native delayed deliveries are not enabled.");
+            }
+
+            var delayedMessageEntity = new DelayedMessageEntity
+            {
+                PartitionKey = DelayedMessageEntity.GetPartitionKey(date),
+                RowKey = $"{DelayedMessageEntity.GetRawRowKeyPrefix(date)}_{Guid.NewGuid():N}",
+            };
+
+            delayedMessageEntity.SetOperation(operation);
+            return delayedMessageStorageTable.ExecuteAsync(TableOperation.Insert(delayedMessageEntity), null, null, cancellationToken);
+        }
+
+        private CloudTable delayedMessageStorageTable;
+        private bool enabled;
+    }
+}
---
+++ src/Transport/NativeDelayedDeliveryProcessor.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.Cosmos.Table;
+using NServiceBus.Logging;
+using NServiceBus.Transport.AzureStorageQueues;
+
+namespace NServiceBus
+{
+    class NativeDelayedDeliveryProcessor
+    {
+        public static NativeDelayedDeliveryProcessor Disabled()
+        {
+            return new NativeDelayedDeliveryProcessor();
+        }
+
+        private NativeDelayedDeliveryProcessor()
+        {
+            enabled = false;
+        }
+
+        public NativeDelayedDeliveryProcessor(
+            Dispatcher dispatcher,
+            CloudTable delayedMessageStorageTable,
+            BlobServiceClient blobServiceClient,
+            ImmutableDictionary<string, string> errorQueueAddresses,
+            TransportTransactionMode transportTransactionMode,
+            BackoffStrategy backoffStrategy,
+            string userDefinedDelayedDeliveryPoisonQueue)
+        {
+            enabled = true;
+            this.dispatcher = dispatcher;
+            this.delayedMessageStorageTable = delayedMessageStorageTable;
+            this.blobServiceClient = blobServiceClient;
+            this.errorQueueAddresses = errorQueueAddresses;
+            this.transportTransactionMode = transportTransactionMode;
+            this.backoffStrategy = backoffStrategy;
+            this.userDefinedDelayedDeliveryPoisonQueue = userDefinedDelayedDeliveryPoisonQueue;
+        }
+
+        public void Start()
+        {
+            if (!enabled)
+            {
+                return;
+            }
+
+            Logger.Debug("Starting delayed delivery poller");
+
+            nativeDelayedMessagesCancellationSource = new CancellationTokenSource();
+
+            var isAtMostOnce = transportTransactionMode == TransportTransactionMode.None;
+            poller = new DelayedMessagesPoller(
+                delayedMessageStorageTable,
+                blobServiceClient,
+                errorQueueAddresses,
+                isAtMostOnce,
+                dispatcher,
+                backoffStrategy);
+            poller.Start(nativeDelayedMessagesCancellationSource.Token);
+        }
+
+        public Task Stop()
+        {
+            Logger.Debug("Stopping delayed delivery poller");
+            nativeDelayedMessagesCancellationSource?.Cancel();
+            return poller != null ? poller.Stop() : Task.CompletedTask;
+        }
+
+        private readonly Dispatcher dispatcher;
+        private CloudTable delayedMessageStorageTable;
+        private readonly BlobServiceClient blobServiceClient;
+        private readonly ImmutableDictionary<string, string> errorQueueAddresses;
+        private readonly TransportTransactionMode transportTransactionMode;
+        private readonly BackoffStrategy backoffStrategy;
+        private readonly string userDefinedDelayedDeliveryPoisonQueue;
+        private bool enabled;
+        private CancellationTokenSource nativeDelayedMessagesCancellationSource;
+
+        static readonly ILog Logger = LogManager.GetLogger<NativeDelayedDeliveryProcessor>();
+        private DelayedMessagesPoller poller;
+    }
+}
