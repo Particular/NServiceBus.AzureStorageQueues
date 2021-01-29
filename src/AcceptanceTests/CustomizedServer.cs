e67bc04bbb147df052db8aba74fb63e9c3546384
mauroservienti <mauro.servienti@gmail.com>
2021-01-29 17:55:02 +0100

    CustomizedServer to provide fully custom transport to ATT

---
+++ src/AcceptanceTests/CustomizedServer.cs
@@ -0,0 +1,12 @@
+﻿namespace NServiceBus.Transport.AzureStorageQueues.AcceptanceTests
+{
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+
+    public class CustomizedServer : DefaultServer
+    {
+        public CustomizedServer(AzureStorageQueueTransport transport)
+        {
+            TransportConfiguration = new ConfigureEndpointAzureStorageQueueTransport(transport);
+        }
+    }
+}
