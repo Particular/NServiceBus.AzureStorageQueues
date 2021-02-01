e687a2720409f9718b024357f27d2439019c6b0a
mauroservienti <mauro.servienti@gmail.com>
2021-02-01 16:43:36 +0100

    Remove not used ReceiverConnectionString constant

--- src/Transport/Config/WellKnownConfigurationKeys.cs
+++ src/Transport/Config/WellKnownConfigurationKeys.cs
@@ -2,9 +2,6 @@
 {
     static class WellKnownConfigurationKeys
     {
-        //TODO: this seems to be used nowhere
-        //public const string ReceiverConnectionString = "Transport.ConnectionString";
-
         public static class DelayedDelivery
         {
             public const string TableName = "Transport.AzureStorageQueue.TableName";
