b6d6d56b65a16836e945f24c108f01e09a84db45
mauroservienti <mauro.servienti@gmail.com>
2021-02-05 09:56:53 +0100

    Remove commented code related to settings merge before getting the serializer

--- src/Transport/AzureStorageQueueTransport.cs
+++ src/Transport/AzureStorageQueueTransport.cs
@@ -236,14 +236,6 @@ namespace NServiceBus
 
             var (definition, serializerSettings) = serializerSettingsTuple;
 
-            // TODO: why were settings merged?
-            // serializerSettings.Merge(settings);
-            //var merge = typeof(SettingsHolder).GetMethod("Merge", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            //merge.Invoke(serializerSettings, new object[]
-            //{
-            //    settings
-            //});
-
             var serializerFactory = definition.Configure(settings);
             var serializer = serializerFactory(mapper);
             return serializer;
