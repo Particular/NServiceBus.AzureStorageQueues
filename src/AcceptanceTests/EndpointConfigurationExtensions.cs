aab73d7fc1313d01d33d461f68f934c5ad7e8a32
mauroservienti <mauro.servienti@gmail.com>
2021-02-19 10:04:18 +0100

    Remove todo and explain why

--- src/AcceptanceTests/EndpointConfigurationExtensions.cs
+++ src/AcceptanceTests/EndpointConfigurationExtensions.cs
@@ -6,7 +6,8 @@
     {
         public static string GetEndpointDefinedErrorQueue(this EndpointConfiguration configuration)
         {
-            //TODO this is kind of a hack because the acceptance testing framework doesn't give any access to the defined error queue.
+            //this is kind of a hack because the acceptance testing framework doesn't give any access to the defined error queue.
+            //there is no point in having this in the Core ATT framework since ASQ is the only transport that needs this.
             return configuration.GetSettings().GetOrDefault<string>(ErrorQueueSettings.SettingsKey);
         }
     }
