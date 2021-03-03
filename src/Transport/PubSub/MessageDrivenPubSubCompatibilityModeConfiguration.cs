e68143775d394de9aa4bd2cf3365fcfcb98a8f30
danielmarbach <daniel.marbach@openplace.net>
2021-03-03 11:56:46 +0100

    Prepare basic acceptance tests and migration mode extensions to get the ball rolling

---
+++ src/Transport/PubSub/MessageDrivenPubSubCompatibilityModeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus
+{
+    using Configuration.AdvancedExtensibility;
+
+    /// <summary>
+    /// Configuration extensions for Message-Driven Pub-Sub compatibility mode
+    /// </summary>
+    public static class MessageDrivenPubSubCompatibilityModeConfiguration
+    {
+        /// <summary>
+        ///     Enables compatibility with endpoints running on message-driven pub-sub
+        /// </summary>
+        /// <param name="routingSettings">The transport to enable pub-sub compatibility on</param>
+        public static SubscriptionMigrationModeSettings EnableMessageDrivenPubSubCompatibilityMode(
+            this RoutingSettings routingSettings)
+        {
+            var settings = routingSettings.GetSettings();
+            settings.Set("NServiceBus.Subscriptions.EnableMigrationMode", true);
+            return new SubscriptionMigrationModeSettings(settings);
+        }
+    }
+}
