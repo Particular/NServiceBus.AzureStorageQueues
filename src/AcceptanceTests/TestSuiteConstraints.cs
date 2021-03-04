8b7a5614499d67ba31689691fc7ae1fb5c9310e0
danielmarbach <daniel.marbach@openplace.net>
2021-03-04 20:19:19 +0100

    Switch contraints

--- src/AcceptanceTests/TestSuiteConstraints.cs
+++ src/AcceptanceTests/TestSuiteConstraints.cs
@@ -6,7 +6,7 @@
     {
         public bool SupportsDtc => false;
         public bool SupportsCrossQueueTransactions => false;
-        public bool SupportsNativePubSub => false;
+        public bool SupportsNativePubSub => true;
         public bool SupportsDelayedDelivery => true;
         public bool SupportsOutbox => false;
         public bool SupportsPurgeOnStartup => true;
