8724b216232389109aeea82be59b7a075dfbfbeb
mauroservienti <mauro.servienti@gmail.com>
2021-03-08 14:19:10 +0100

    Transport shim API for legacy compatibility

--- src/Transport/Config/AzureStorageTransportExtensions.cs
+++
@@ -1,8 +0,0 @@
-namespace NServiceBus
-{
-    /// <summary>Extension methods for <see cref="AzureStorageQueueTransport"/>.</summary>
-    public static partial class AzureStorageTransportExtensions
-    {
-
-    }
-}
