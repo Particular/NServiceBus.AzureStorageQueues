45dd0447ebc7598b5be16b46bd008a85066fce09
danielmarbach <daniel.marbach@openplace.net>
2022-10-31 15:54:28 +0100

    Rename obsoletes file

---
+++ src/Transport/obsoletes-v12.cs
@@ -0,0 +1,19 @@
+﻿#pragma warning disable 1591
+
+namespace NServiceBus
+{
+    using System;
+
+    partial class AccountRoutingSettings
+    {
+        [ObsoleteEx(
+            Message =
+                "Account aliases using connection strings have been deprecated. Use the AddAccount overload that accepts a QueueServiceClient instance.",
+            TreatAsErrorFromVersion = "12.0",
+            RemoveInVersion = "13.0")]
+        public AccountInfo AddAccount(string alias, string connectionString)
+            => throw new NotImplementedException();
+    }
+}
+
+#pragma warning restore 1591
