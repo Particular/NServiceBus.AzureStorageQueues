e8779f15f237b71037ce06d01bfa0ff29ca5ee09
Daniel Marbach <daniel.marbach@openplace.net>
2022-11-07 08:12:35 +0100

    Bring back UseTableName otherwise that fluent setting could no longer be used (#903)
    # Conflicts:
    #	src/Transport/Config/AzureStorageTransportExtensions.cs

--- src/Transport/Config/DelayedDeliverySettings.cs
+++ src/Transport/Config/DelayedDeliverySettings.cs
@@ -6,22 +6,19 @@ namespace NServiceBus
     /// <summary>Configures native delayed delivery.</summary>
     public partial class DelayedDeliverySettings : ExposeSettings
     {
-        NativeDelayedDeliverySettings transportDelayedDelivery;
+        readonly NativeDelayedDeliverySettings transportDelayedDelivery;
 
         internal DelayedDeliverySettings(NativeDelayedDeliverySettings transportDelayedDelivery) : base(new SettingsHolder())
-        {
-            this.transportDelayedDelivery = transportDelayedDelivery;
-        }
+            => this.transportDelayedDelivery = transportDelayedDelivery;
 
         /// <summary>Override the default table name used for storing delayed messages.</summary>
         /// <param name="delayedMessagesTableName">New table name.</param>
-        [ObsoleteEx(
+        [PreObsolete(
             Message = "Configure the transport via the AzureStorageQueueTransport DelayedDelivery.DelayedDeliveryTableName property",
             TreatAsErrorFromVersion = "12.0",
-            RemoveInVersion = "13.0")]
+            RemoveInVersion = "13.0",
+            Note = AzureStorageTransportExtensions.Note)]
         public void UseTableName(string delayedMessagesTableName)
-        {
-            transportDelayedDelivery.DelayedDeliveryTableName = delayedMessagesTableName;
-        }
+            => transportDelayedDelivery.DelayedDeliveryTableName = delayedMessagesTableName;
     }
 }
