9facb371a847be4b31cc96446b3ec48e1626b06f
Tamara Rivera <tamita.rivera@gmail.com>
2025-08-07 16:08:51 -0700

    Remove custom PreObsolete attribute

--- src/Transport/MessageWrapper.cs
+++ src/Transport/MessageWrapper.cs
@@ -2,6 +2,7 @@ namespace NServiceBus.Azure.Transports.WindowsAzureStorageQueues
 {
     using System;
     using System.Collections.Generic;
+    using Particular.Obsoletes;
 
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
@@ -17,7 +18,8 @@ namespace NServiceBus.Azure.Transports.WindowsAzureStorageQueues
         public string ReplyToAddress { get; set; }
 
         // Defining an obsoletion path for this has been raised on https://github.com/Particular/NServiceBus.AzureStorageQueues/issues/1318
-        [Obsolete("Legacy property for backwards compatibility.", error: false)]
+        [ObsoleteMetadata(TreatAsErrorFromVersion = "99", RemoveInVersion = "100")]
+        [Obsolete("Will be treated as an error from version 99.0.0. Will be removed in version 100.0.0.", false)]
         public TimeSpan TimeToBeReceived { get; set; }
 
         public Dictionary<string, string> Headers { get; set; }
