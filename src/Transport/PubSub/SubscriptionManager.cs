d2edd0b9b9e1ead4d25586741fee7a095873942c
Dharmishtha Patel <dharmishthapatel@Dharmishthas-MacBook-Pro.local>
2026-03-23 11:56:00 -0700

    add manifest information to diagnostics

--- src/Transport/PubSub/SubscriptionManager.cs
+++ src/Transport/PubSub/SubscriptionManager.cs
@@ -1,6 +1,7 @@
 namespace NServiceBus.Transport.AzureStorageQueues
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Extensibility;
@@ -11,16 +12,22 @@ namespace NServiceBus.Transport.AzureStorageQueues
         readonly ISubscriptionStore subscriptionStore;
         readonly string localAddress;
         readonly string endpointName;
+        readonly StartupDiagnosticEntries startupDiagnostic;
 
-        public SubscriptionManager(ISubscriptionStore subscriptionStore, string endpointName, string localAddress)
+        public SubscriptionManager(ISubscriptionStore subscriptionStore, string endpointName, string localAddress, StartupDiagnosticEntries startupDiagnostic)
         {
             this.endpointName = endpointName;
             this.localAddress = localAddress;
             this.subscriptionStore = subscriptionStore;
+            this.startupDiagnostic = startupDiagnostic;
         }
 
         public Task SubscribeAll(MessageMetadata[] eventTypes, ContextBag context, CancellationToken cancellationToken = default)
         {
+            startupDiagnostic.Add("Manifest-Subscriptions", eventTypes
+                .Select(eventType => eventType.MessageType.FullName)
+                .ToArray());
+
             var tasks = new List<Task>(eventTypes.Length);
             foreach (var eventType in eventTypes)
             {
